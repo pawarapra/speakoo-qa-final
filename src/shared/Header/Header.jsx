ea032ed83310782fcd763d4ef7c307e33cce8ae1
jpmarinato <148399011+jpmarinato@users.noreply.github.com>
2024-04-15 19:40:26 -0700

    Reapply "fix all issues"
    This reverts commit 89f26ac13c3ee8046354194379b06bc2ff5acc6e.

--- src/shared/Header/Header.jsx
+++ src/shared/Header/Header.jsx
@@ -14,49 +14,58 @@ export default function Header() {
 
   return (
     <div className='container'>
-      <div className='hometop'>
+      <div className='grid'>
+        <div className='col-0 col-1-sm col-2-md col-3-lg'></div>
+        <div className='col-12 col-10-sm col-8-md col-6-lg contain'>
+          <div className='hometop'>
 
-      <img src={headimg} alt="Image" className="head-image" />
+          <img src={headimg} alt="Image" className="head-image" />
 
-      </div>
+          </div>
+          <div className='grid'>
+            <div className='col-1'></div>
+            <div className='homebottom container-inner col-10'>
+            <div className="grid">
+              <Link to="alpha" className={'homesquare col-6 alphacolor'} style={{ textDecoration: 'none', display: 'flex', justifyContent: 'center', alignItems: 'center' }}>
+                <div className="image-container">
+                  <img src={alphaimg} alt="Image" className="centered-image" />
 
-      <div className='homebottom container-inner'>
-        <div className="grid">
-          <Link to="alpha" className={'homesquare col-6 alphacolor'} style={{ textDecoration: 'none', display: 'flex', justifyContent: 'center', alignItems: 'center' }}>
-            <div className="image-container">
-              <img src={alphaimg} alt="Image" className="centered-image" />
+                <span className="link-text">
+                  Basic <br />Conversation
+                </span>
+                </div>
+              </Link>
 
-            <span className="link-text">
-              Basic <br />Conversations
-            </span>
+              <Link to="beta" className={'homesquare col-6 betacolor'} style={{ textDecoration: 'none', display: 'flex', justifyContent: 'center', alignItems: 'center' }}>
+                <div className="image-container">
+                  <img src={betaimg} alt="Beta Image" className="centered-image" />
+
+                <span className="link-text">Emotions</span>
+                </div>
+              </Link>
+
+              <Link to="gamma" className={'homesquare col-6 gammacolor'} style={{ textDecoration: 'none', display: 'flex', justifyContent: 'center', alignItems: 'center' }}>
+                <div className="image-container">
+                  <img src={gammaimg} alt="Gamma Image" className="centered-image" />
+
+                <span className="link-text">Food</span>            
+                </div>
+              </Link>
+
+              <Link to="fav" className={'homesquare col-6 favcolor'} style={{ textDecoration: 'none', display: 'flex', justifyContent: 'center', alignItems: 'center' }}>
+                <div className="image-container">
+                  <img src={favimg} alt="Fav Image" className="centered-image" />
+
+                <span className="link-text">Favorites</span>
+                </div>
+              </Link>
             </div>
-          </Link>
-
-          <Link to="beta" className={'homesquare col-6 betacolor'} style={{ textDecoration: 'none', display: 'flex', justifyContent: 'center', alignItems: 'center' }}>
-            <div className="image-container">
-              <img src={betaimg} alt="Beta Image" className="centered-image" />
-
-            <span className="link-text">Emotions</span>
-            </div>
-          </Link>
-
-          <Link to="gamma" className={'homesquare col-6 gammacolor'} style={{ textDecoration: 'none', display: 'flex', justifyContent: 'center', alignItems: 'center' }}>
-            <div className="image-container">
-              <img src={gammaimg} alt="Gamma Image" className="centered-image" />
-
-            <span className="link-text">Food</span>            
-            </div>
-          </Link>
-
-          <Link to="fav" className={'homesquare col-6 favcolor'} style={{ textDecoration: 'none', display: 'flex', justifyContent: 'center', alignItems: 'center' }}>
-            <div className="image-container">
-              <img src={favimg} alt="Fav Image" className="centered-image" />
-
-            <span className="link-text">Favorite</span>
-            </div>
-          </Link>
+          </div>
+        </div>
         </div>
       </div>
+
+      
     </div>
   );
 }
